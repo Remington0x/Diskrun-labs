3601bb305825b26a663e6cbff1fde7c9c3e7107e
alext <sir.lex-ivan@ya.ru>
2022-01-13 03:13:40 +0300

    Made that work

--- lab5/solution/main.cpp
+++ lab5/solution/main.cpp
@@ -1,14 +1,22 @@
-#include <iostream>
-#include "ukkonen.hpp"
+#include "suffix_tree.hpp"
+
+const char SENTINEL1 = '$';
+const char SENTINEL2 = '*';
 
 int main() {
-    TSuffTree* tree = new TSuffTree();
-
     std::string s;
     std::cin >> s;
-    tree->Fill(s);
-    tree->Print();
-
-    delete tree;
+    std::string t = s + SENTINEL1;
+    std::cin >> s;
+    t = t + s + SENTINEL2;
+    TSuffixTree st(t);
+    
+    //st.Print();
+    //std::cout << "\n\n";
+    st.Find(SENTINEL1, SENTINEL2);
+    // std::cin >> s >> t;
+    // std::cout << StringComparison((void*)&s, (void*)&t);
+    //std::cout << st.RecMaxDepth(SENTINEL1, SENTINEL2, 0, 0) << std::endl;
+    //std::cout << st.LexMinString(s.size()) << '\n';
     return 0;
 }
